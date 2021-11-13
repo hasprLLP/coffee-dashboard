5b779775f9d02690f688b7b82c91312c34ddaf8d
blitzve0 <frustasio@gmail.com>
2021-11-14 01:39:46 +0530

    responsive input

--- src/components/input.js
+++ src/components/input.js
@@ -1,13 +1,15 @@
 import { Input } from "@chakra-ui/react";
+import useSize from "@/utilities/useSize";
 
 const TextField = ({ title, placeholder, value, setter }) => {
+  const size = useSize();
   return (
     <div className="input">
       <div className="input-title">{title}</div>
       <Input
         value={value}
         onChange={(e) => setter(e.target.value)}
-        size="md"
+        size={size > 1600 ? "lg" : size > 1280 ? "md" : size > 1024 ? "sm" : "xs"}
         bg="rgba(242, 242, 242, 0.4)"
         focusBorderColor="#38B2AC"
         marginBottom="2vw"
