79ed0d00060c6e6c4945a70c5e4b510ee8e1279a
blitzve0 <frustasio@gmail.com>
2021-11-14 01:23:31 +0530

    font size input

--- src/components/input.js
+++ src/components/input.js
@@ -1,7 +1,6 @@
 import { Input } from "@chakra-ui/react";
 
 const TextField = ({ title, placeholder, value, setter }) => {
-
   return (
     <div className="input">
       <div className="input-title">{title}</div>
@@ -13,6 +12,7 @@ const TextField = ({ title, placeholder, value, setter }) => {
         focusBorderColor="#38B2AC"
         marginBottom="2vw"
         marginTop="1vw"
+        fontSize="0.9vw"
         placeholder={placeholder}
       />
     </div>
