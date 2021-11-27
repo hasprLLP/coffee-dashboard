4e29fc97f525176a52b68ea11982086db613b2b5
luv jeri <hiluvjeri@gmail.com>
2021-11-27 16:08:23 +0530

    -

--- pages/route/index.js
+++ pages/route/index.js
@@ -1,35 +1,40 @@
 //& Input Components [#IMPORTS#]
-import SimpleCard from "@/components/simplecard";
-import TextField from "@/components/input";
-import SaveButton from "@/components/button";
-import { useState } from "react";
+import RouteCard from '@/components/routeCard';
+import TextField from '@/components/input';
+import server from '@/functions/server';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 //& Create & Export Driver [#FUNCTION#]
 export default function ViewRoute() {
-  const [routeName, setRouteName] = useState("");
+  const router = useRouter();
+  const [routeName, setRouteName] = useState('');
+  //$ Getting all the routes
+  const [routes, setRoutes] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await server.get(`/route?populate=school,bus`);
+      setRoutes(data.data);
+    };
 
-  //$ States and Hooks [#STATES#]
-  const fields = [
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-    { name: "Route Name", school: "DMA School", start: "Starting Point", end: "Destination Point", type: "route" },
-  ];
+    fetch();
+  }, []);
+
+  const onEdit = (id, data) => {
+    router.push({ pathname: `/route/${id}`, query: { data: JSON.stringify(data) } });
+  };
+  const onDetails = (id, data) => {
+    router.push({ pathname: `/route/details/${id}`, query: { data: JSON.stringify(data) } });
+  };
 
   //& Return UI [#RETURN#]
   return (
-    <div className="home" style={{ backgroundColor: "var(--chakra-colors-gray-100)" }}>
-      <div className="driver">
-        <TextField title={"Search Route Name"} placeholder={"Type Route Details"} value={routeName} setter={setRouteName} color={"white"} />
-        <div className="driver-form" style={{ justifyContent: "flex-start" }}>
-          {fields.map((item, i) => {
-            return <SimpleCard key={i} name={item.name} school={item.school} start={item.start} end={item.end} type={item.type} />;
+    <div className='home' style={{ backgroundColor: 'var(--chakra-colors-gray-100)' }}>
+      <div className='driver'>
+        <TextField title={'Search Route Name'} placeholder={'Type Route Details'} value={routeName} setter={setRouteName} color={'white'} />
+        <div className='driver-form' style={{ justifyContent: 'flex-start' }}>
+          {routes.map((route, i) => {
+            return <RouteCard key={i} id={route._id} route={route} onEdit={onEdit} onDetails={onDetails} />;
           })}
         </div>
       </div>
