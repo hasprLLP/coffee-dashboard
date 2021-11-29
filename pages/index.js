14ee5bb19d2a93ebd1f991cf5bc8a00e333463d5
blitzve0 <frustasio@gmail.com>
2021-11-29 17:08:44 +0530

    new logo

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import FilePicker from "@/components/filepicker";
 import axios from "axios";
 import { useState } from "react";
 axios.defaults.withCredentials = true;
 
 export default function Home() {
-  const [photo, setPhoto] = useState("");
 
   return (
     <div className="home">
       <div style={{ marginLeft: "5vw", width: "85%", height: "100%", marginTop: "5vw" }}>
         Dashboard
-        <FilePicker value={photo} setter={setPhoto} />
       </div>
     </div>
   );
