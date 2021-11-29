8a5a51abc0d03ff0ab0a929784c1b0734298143e
blitzve0 <frustasio@gmail.com>
2021-11-29 15:11:36 +0530

    push to vercel

--- src/components/filepicker.js
+++ src/components/filepicker.js
@@ -1,19 +1,19 @@
 const FilePicker = ({ title, setter }) => {
-
   const onFilePick = (e) => {
     const file = e.target.files[0];
-    setter(file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+      setter(`data:image/png;base64,${base64String}`);
+    };
+    reader.readAsDataURL(file);
   };
 
   return (
     <div className="dropdown" style={{ marginRight: "0.5vw" }}>
       <div className="dropdown-title">{title}</div>
-      <input
-        type="file"
-        className="file-picker css-13vuage"
-        onChange={onFilePick}
-        style={{ marginBottom: "2vw", marginTop: "1vw" }}
-      />
+      <input type="file" className="file-picker css-13vuage" onChange={onFilePick} style={{ marginBottom: "2vw", marginTop: "1vw" }} />
     </div>
   );
 };
