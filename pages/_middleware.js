648ed2d1fade9d45544091e6e088cdee438fe9c7
luv jeri <hiluvjeri@gmail.com>
2021-11-29 21:28:33 +0530

    Middleware Test Push

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,38 +1,46 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(req, res) {
-  try {
-    let data = {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: req.cookies.jwt,
-      },
-    };
-    const { pathname } = req.nextUrl;
-
-    if (pathname !== '/login') {
-      const is_auth = await fetch(process.env.SERVER_URL + 'admin/authentication/verify', data);
-
-      if (is_auth.status === 200) {
-        return NextResponse.next();
-      } else {
-        return NextResponse.redirect('/login');
-      }
+  const is_auth = await fetch('https://jsonplaceholder.typicode.com/posts/1', { method: 'GET' });
+  console.log(is_auth.status);
+  const { pathname } = req.nextUrl;
+  if (pathname !== '/login') {
+    if (is_auth.status !== 200) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect('/login');
     }
-
-    if (pathname === '/login') {
-      const is_auth = await fetch(process.env.SERVER_URL + 'admin/authentication/verify', data);
-      console.log(is_auth);
-      if (is_auth.status === 200) {
-        return NextResponse.redirect('/');
-      } else {
-        return NextResponse.next();
-      }
-    }
-  } catch (error) {
-    console.log(error);
-    return NextResponse.next();
   }
+  // try {
+  //   let data = {
+  //     method: 'POST',
+  //     credentials: 'include',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       authorization: req.cookies.jwt,
+  //     },
+  //   };
+  //   const { pathname } = req.nextUrl;
+
+  //   if (pathname !== '/login') {
+  //     if (is_auth.status === 200) {
+  //       return NextResponse.next();
+  //     } else {
+  //       return NextResponse.redirect('/login');
+  //     }
+  //   }
+
+  //   if (pathname === '/login') {
+  //     const is_auth = await fetch(process.env.SERVER_URL + 'admin/authentication/verify', data);
+  //     console.log(is_auth);
+  //     if (is_auth.status === 200) {
+  //       return NextResponse.redirect('/');
+  //     } else {
+  //       return NextResponse.next();
+  //     }
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  //   return NextResponse.next();
+  // }
 }
