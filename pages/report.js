200e6c539761ecf45a9dae3d9d63d7d8f52ada38
blitzve0 <frustasio@gmail.com>
2021-11-29 16:11:31 +0530

    report statng

--- pages/report.js
+++ pages/report.js
@@ -10,7 +10,7 @@ export default function Report() {
   return (
     <div className="home">
       <div style={{ marginLeft: "5vw", width: "85%", height: "100%", marginTop: "5vw" }}>
-        <DropDown title={"Select Report"} options={["Monthly Report", "Fee Report", "More Adding"]} value={report} setter={setReport} />
+        <DropDown title={"Select Report"} options={["Total Fee View", "Monthly Fee View", "Fee Invoices","Total Commission View","Monthly Commission View"]} value={report} setter={setReport} />
         <div style={{paddingBottom: "2vw"}}><Table /></div>
       </div>
     </div>
