c4d7da44ac40e692b2cea7d70caf46c65773bce1
luv jeri <hiluvjeri@gmail.com>
2021-11-30 02:06:35 +0530

    Save

--- src/components/saveButton.js
+++ src/components/saveButton.js
@@ -2,12 +2,7 @@ import { Button } from '@chakra-ui/react';
 import Notification from '@/components/notification';
 import axios from 'axios';
 import { useState } from 'react';
-
-const server = axios.create({
-  baseURL: `${process.env.SERVER_URL}`,
-  timeout: 10000,
-  withCredentials: true,
-});
+import server from '@/functions/server';
 
 const SaveButton = ({ collection, data }) => {
   const [status, setStatus] = useState(null);
