9b06e29d71aeb48f9989bd14c3a12f0e56814267
luv jeri <hiluvjeri@gmail.com>
2021-11-29 13:23:51 +0530

    Added Driver

--- pages/driver/add.jsx
+++ pages/driver/add.jsx
@@ -1,54 +1,65 @@
 //& Input Components [#IMPORTS#]
-import TextField from "@/components/input";
-import DropDown from "@/components/dropdown";
-import FilePicker from "@/components/filepicker";
-import SaveButton from "@/components/saveButton";
-import { useState } from "react";
-
+import TextField from '@/components/input';
+import DropDown from '@/components/dropdown';
+import FilePicker from '@/components/filepicker';
+import SaveButton from '@/components/saveButton';
+import { useState } from 'react';
+import server from '@/functions/server';
 //& Create & Export Driver [#FUNCTION#]
 export default function Create() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const [phone, setPhone] = useState("");
-  const [phone2, setPhone2] = useState("");
-  const [school, setSchool] = useState("");
-  const [photo, setPhoto] = useState("");
-  const [sign, setSign] = useState("");
-  const [dl, setDl] = useState("");
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
+  const [pin, setPin] = useState();
+  const [photo, setPhoto] = useState();
+  const [sign, setSign] = useState();
+  const [drivingLicense, setDrivingLicense] = useState();
 
   //$ States and Hooks [#STATES#]
   const fields = [
-    { title: "Driver Name", isRequired: true,placeholder: "Bus Operator Name", value: name, setter: setName },
-    { title: "Date of Birth", placeholder: "ex 09/11/2021", value: age, setter: setAge, type: "date" },
-    { title: "Mobile No",isRequired: true, placeholder: "Operator Phone no", type: "number", value: phone, setter: setPhone, type: "tel", prefix: "+91" },
-    { title: "Landline (Optional)", placeholder: "Additional Contact no", value: phone2, setter: setPhone2 },
-    { title: "School Name", options: ["DMA", "St Mary", "Joseph"], value: school, setter: setSchool, type: "dropdown" },
-    { title: "Upload Passport Photo", value: photo, setter: setPhoto, type: "upload" },
-    { title: "Upload Signature", value: sign, setter: setSign, type: "upload" },
-    { title: "Upload Driving License", value: dl, setter: setDl, type: "upload" },
+    { title: 'Driver Name', isRequired: true, placeholder: 'Bus Operator Name', value: name, setter: setName },
+    {
+      title: 'Mobile No',
+      isRequired: true,
+      placeholder: 'Operator Phone no',
+      type: 'number',
+      value: phone,
+      setter: setPhone,
+      type: 'tel',
+      prefix: '+91',
+    },
+    { title: 'Pin', placeholder: 'Pin', value: pin, setter: setPin },
+    { title: 'Upload Passport Photo', value: photo, setter: setPhoto, type: 'upload' },
+    { title: 'Upload Signature', value: sign, setter: setSign, type: 'upload' },
+    { title: 'Upload Driving License', value: drivingLicense, setter: setDrivingLicense, type: 'upload' },
   ];
 
   // FIXME:className 'driver', 'layout-form' & 'layout-title' are same for most of the pages, make something like className - 'title' , 'form' & 'container'
   //& Return UI [#RETURN#]
   return (
-    <div className="home">
-      <div className="home-shift">
-        <div className="layout-title">Add Driver</div>
-        <div className="layout-form" style={{ justifyContent: "flex-start" }}>
+    <div className='home'>
+      <div className='home-shift'>
+        <div className='layout-title'>Add Driver</div>
+        <div className='layout-form' style={{ justifyContent: 'flex-start' }}>
           {fields.map((item, i) => {
-            return item.type === "dropdown" ? (
+            return item.type === 'dropdown' ? (
               <DropDown key={i} title={item.title} options={item.options} value={item.value} setter={item.setter} />
-            ) : item.type === "upload" ? (
+            ) : item.type === 'upload' ? (
               <FilePicker title={item.title} value={item.value} setter={item.setter} />
             ) : (
-              <TextField key={i} isRequired={item.isRequired} title={item.title} placeholder={item.placeholder} value={item.value} setter={item.setter} prefix={item.prefix} type={item.type} />
+              <TextField
+                key={i}
+                isRequired={item.isRequired}
+                title={item.title}
+                placeholder={item.placeholder}
+                value={item.value}
+                setter={item.setter}
+                prefix={item.prefix}
+                type={item.type}
+              />
             );
           })}
         </div>
-        <SaveButton
-          collection={"bus"}
-          // data={{ name, busNumber, capacity }}
-        />
+        <SaveButton collection={'admin/operator'} data={{ name, phone, pin, sign, photo, drivingLicense }} />
       </div>
     </div>
   );
--- pages/passenger/index.jsx
+++ pages/passenger/index.jsx
@@ -1,20 +1,20 @@
 //& Input Components [#IMPORTS#]
-import PhotoCard from "@/components/photoCard";
-import { useState, useEffect } from "react";
-import server from "functions/server";
-import TextField from "@/components/input";
-import Fuse from "fuse.js"
-import { useRouter } from "next/router";
+import PhotoCard from '@/components/photoCard';
+import { useState, useEffect } from 'react';
+import server from 'functions/server';
+import TextField from '@/components/input';
+import Fuse from 'fuse.js';
+import { useRouter } from 'next/router';
 
 //& Create & Export Driver [#FUNCTION#]
 export default function Passenger() {
-  const [student, setStudent] = useState("");
+  const [student, setStudent] = useState('');
 
   const router = useRouter();
   const [passengers, setPassengers] = useState([]);
   useEffect(() => {
     const fetch = async () => {
-      const { data } = await server.get(`/passenger?populate=route`);
+      const { data } = await server.get(`/passenger`);
       setPassengers(data.data);
     };
 
@@ -29,7 +29,7 @@ export default function Passenger() {
     router.push({ pathname: `/passenger/report/${id}`, query: { data: JSON.stringify(data) } });
   };
 
-    //& Fuse JS [#FUSE#]
+  //& Fuse JS [#FUSE#]
   //$ New Fuse Instance with Settings
   const fuse = new Fuse(passengers, {
     isCaseSensitive: false,
@@ -38,19 +38,19 @@ export default function Passenger() {
     includeMatches: true,
     findAllMatches: true,
     minMatchCharLength: 0,
-    keys: ["name", "guardian.name", "phone","DOB"],
+    keys: ['name', 'guardian.name', 'phone', 'DOB'],
   });
 
-  const result = student !== "" && fuse.search(student);
+  const result = student !== '' && fuse.search(student);
   const resultFilter = result && result.map((result) => result.item);
   const searchResultDisplay = resultFilter || passengers;
 
   //& Return UI [#RETURN#]
   return (
-    <div className="home" style={{ backgroundColor: "var(--chakra-colors-gray-100)" }}>
-      <div className="home-shift">
-        <TextField title={"Search Student Name"} placeholder={"Type student name"} value={student} setter={setStudent} color={"white"} />
-        <div className="layout-form" style={{ justifyContent: "flex-start" }}>
+    <div className='home' style={{ backgroundColor: 'var(--chakra-colors-gray-100)' }}>
+      <div className='home-shift'>
+        <TextField title={'Search Student Name'} placeholder={'Type student name'} value={student} setter={setStudent} color={'white'} />
+        <div className='layout-form' style={{ justifyContent: 'flex-start' }}>
           {searchResultDisplay.map((passenger, i) => {
             return <PhotoCard key={i} id={passenger.id} onEdit={onEdit} onDetail={onDetail} passenger={passenger} />;
           })}
