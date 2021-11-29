67d5429fa8fd95ed4a5de6f0535a7bfee6e018f3
blitzve0 <frustasio@gmail.com>
2021-11-29 16:37:25 +0530

    1 error left

--- pages/school/report/[report].jsx
+++ pages/school/report/[report].jsx
@@ -6,14 +6,23 @@ import { JSONToHTMLTable } from "@kevincobain2000/json-to-html-table";
 export default function Details() {
   const router = useRouter();
   const { id } = router.query;
-  const data = JSON.parse(router.query.data);
 
-  // FIXME:className 'driver', 'layout-form' & 'layout-title' are same for most of the pages, make something like className - 'title' , 'form' & 'container'
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (router?.query?.data) {
+      setData(JSON.parse(router.query.data));
+    }
+  }, [router.query.data, data]);
+
   //& Return UI [#RETURN#]
   return (
     <div className="home">
       <div className="home-shift">
-        <div className="layout-title"><GoBack />Details </div>
+        <div className="layout-title">
+          <GoBack />
+          Details{" "}
+        </div>
         <div style={{ textTransform: "capitalize" }} className="table-report">
           {data && <JSONToHTMLTable data={data} />}
         </div>
