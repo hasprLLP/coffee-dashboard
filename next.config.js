a0f4ad8a0f2bfbad84d5b60cd732aae3492c80c4
luv jeri <hiluvjeri@gmail.com>
2022-01-06 11:20:18 +0530

    -

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   env: {
     appName: 'Coffee',
     JWT_SECRET: 'Coffee',
-    // SERVER_URL: 'http://localhost:8080/api/v1/',
-    SERVER_URL: 'https://coffeecenterbus.herokuapp.com/api/v1/',
+    SERVER_URL: 'http://localhost:8080/api/v1/',
+    // SERVER_URL: 'https://coffeecenterbus.herokuapp.com/api/v1/',
     NEXT_PUBLIC_GOOGLE_ANALYTICS: 'G-VXB0G37H90',
     MAP_KEY: 'AIzaSyCJa0NdXTwLt2IwUyHhj1Gzy0Fe56vP_G8',
     IDENTITY_KEY: 'AIzaSyC3E418LZBq8oU8B6vLIdN_K8g2fOrh1B4',
@@ -17,4 +17,4 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
   },
   swcMinify: true,
-})
+});
