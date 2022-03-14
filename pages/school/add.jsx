a6c2dba4424a61352df7f39b96e062cdcb90ae1b
blitzve0 <frustasio@gmail.com>
2022-03-14 10:23:26 +0530

    location format - fixed

--- pages/school/add.jsx
+++ pages/school/add.jsx
@@ -1,21 +1,20 @@
 //& Input Components [#IMPORTS#]
-import TextField from '@/components/input';
-import DropDown from '@/components/dropdown';
-import SaveButton from '@/components/saveButton';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import TextField from '@/components/input'
+import DropDown from '@/components/dropdown'
+import SaveButton from '@/components/saveButton'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 //& Create & Export Driver [#FUNCTION#]
 export default function Create() {
-  const [name, setName] = useState();
-  const [city, setCity] = useState();
-  const [prefix, setPrefix] = useState();
-  const [address, setAddress] = useState();
-  const [zip, setZip] = useState();
-  const [phone, setPhone] = useState();
+  const [name, setName] = useState()
+  const [city, setCity] = useState()
+  const [prefix, setPrefix] = useState()
+  const [address, setAddress] = useState()
+  const [zip, setZip] = useState()
+  const [phone, setPhone] = useState()
 
-  const setterArray = [setName, setCity, setAddress, setZip, setPhone, setPrefix];
-
+  const setterArray = [setName, setCity, setAddress, setZip, setPhone, setPrefix]
 
   //$ States and Hooks [#STATES#]
   const fields = [
@@ -25,14 +24,14 @@ export default function Create() {
     { title: 'Address', isRequired: true, placeholder: 'Address of the school', value: address, setter: setAddress },
     { title: 'Zip Code', type: 'number', placeholder: 'Enter Zip Code', value: zip, setter: setZip },
     { title: 'Phone', placeholder: 'School Contact Number', type: 'tel', prefix: '+91', value: phone, setter: setPhone },
-  ];
+  ]
 
   //& Return UI [#RETURN#]
   return (
-    <div className='home'>
-      <div className='home-shift'>
-        <div className='layout-title'>Add School</div>
-        <div className='layout-form' style={{ justifyContent: 'flex-start' }}>
+    <div className="home">
+      <div className="home-shift">
+        <div className="layout-title">Add School</div>
+        <div className="layout-form" style={{ justifyContent: 'flex-start' }}>
           {fields.map((item, i) => {
             return item.type === 'dropdown' ? (
               <DropDown key={i} title={item.title} options={item.options} value={item.value} setter={item.setter} />
@@ -47,7 +46,7 @@ export default function Create() {
                 prefix={item.prefix}
                 isRequired={item.isRequired}
               />
-            );
+            )
           })}
         </div>
         <SaveButton
@@ -61,12 +60,12 @@ export default function Create() {
             phone,
             location: {
               type: 'Point',
-              coordinates: [23.854080641497234, 78.7799817655712],
+              coordinates: [0, 0],
               address: address,
             },
           }}
         />
       </div>
     </div>
-  );
+  )
 }
--- pages/school/edit/[id].jsx
+++ pages/school/edit/[id].jsx
@@ -1,45 +1,45 @@
 //& Input Components [#IMPORTS#]
-import TextField from "@/components/input";
-import DropDown from "@/components/dropdown";
-import GoBack from "@/helpers/goback";
-import UpdateButton from "@/components/updateButton";
-import DeleteButton from "@/components/deleteButton";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import TextField from '@/components/input'
+import DropDown from '@/components/dropdown'
+import GoBack from '@/helpers/goback'
+import UpdateButton from '@/components/updateButton'
+import DeleteButton from '@/components/deleteButton'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 //& Create & Export Driver [#FUNCTION#]
 export default function EditSchool() {
-  const router = useRouter();
-  const { id } = router.query;
+  const router = useRouter()
+  const { id } = router.query
 
-  const [name, setName] = useState();
-  const [city, setCity] = useState();
-  const [prefix, setPrefix] = useState();
-  const [address, setAddress] = useState();
-  const [zip, setZip] = useState();
-  const [phone, setPhone] = useState();
+  const [name, setName] = useState()
+  const [city, setCity] = useState()
+  const [prefix, setPrefix] = useState()
+  const [address, setAddress] = useState()
+  const [zip, setZip] = useState()
+  const [phone, setPhone] = useState()
 
   useEffect(() => {
     if (router.query.data) {
-      const data = JSON.parse(router.query.data);
-      setName(data?.name);
-      setPrefix(data?.prefix);
-      setCity(data?.city);
-      setAddress(data?.location?.address);
-      setPhone(data?.phone);
-      setZip(data?.zip);
+      const data = JSON.parse(router.query.data)
+      setName(data?.name)
+      setPrefix(data?.prefix)
+      setCity(data?.city)
+      setAddress(data?.location?.address)
+      setPhone(data?.phone)
+      setZip(data?.zip)
     }
-  }, [router.query.data]);
+  }, [router.query.data])
 
   //$ States and Hooks [#STATES#]
   const fields = [
-    { title: "School Name", isRequired: true, placeholder: "Enter School Name", value: name, setter: setName },
-    { title: "School Prefix (for Student ID)", type: "fix", isRequired: true, placeholder: "Enter Prefix", value: prefix, setter: setPrefix, },
-    { title: "City", isRequired: true, placeholder: "City of the school", value: city, setter: setCity },
-    { title: "Address", isRequired: true, placeholder: "Address of the school", value: address, setter: setAddress },
-    { title: "Zip Code", isRequired: true, type: "number", placeholder: "Enter Zip Code", value: zip, setter: setZip },
-    { title: "Phone", placeholder: "School Contact Number", type: "tel", prefix: "+91", value: phone, setter: setPhone },
-  ];
+    { title: 'School Name', isRequired: true, placeholder: 'Enter School Name', value: name, setter: setName },
+    { title: 'School Prefix (for Student ID)', type: 'fix', isRequired: true, placeholder: 'Enter Prefix', value: prefix, setter: setPrefix },
+    { title: 'City', isRequired: true, placeholder: 'City of the school', value: city, setter: setCity },
+    { title: 'Address', isRequired: true, placeholder: 'Address of the school', value: address, setter: setAddress },
+    { title: 'Zip Code', isRequired: true, type: 'number', placeholder: 'Enter Zip Code', value: zip, setter: setZip },
+    { title: 'Phone', placeholder: 'School Contact Number', type: 'tel', prefix: '+91', value: phone, setter: setPhone },
+  ]
 
   //& Return UI [#RETURN#]
   return (
@@ -49,9 +49,9 @@ export default function EditSchool() {
           <GoBack />
           Modify School Details
         </div>
-        <div className="layout-form" style={{ justifyContent: "flex-start" }}>
+        <div className="layout-form" style={{ justifyContent: 'flex-start' }}>
           {fields.map((item, i) => {
-            return item.type === "dropdown" ? (
+            return item.type === 'dropdown' ? (
               <DropDown key={i} title={item.title} options={item.options} value={item.value} setter={item.setter} />
             ) : (
               <TextField
@@ -64,7 +64,7 @@ export default function EditSchool() {
                 prefix={item.prefix}
                 isRequired={item.isRequired}
               />
-            );
+            )
           })}
         </div>
         <div className="layout-edit-row">
@@ -77,19 +77,18 @@ export default function EditSchool() {
               zip,
               phone,
               location: {
-                type: "Point",
                 type: 'Point',
-                coordinates: [],
+                coordinates: [0, 0],
                 address,
               },
             }}
           />
           <DeleteButton
-            collection={"bus"}
+            collection={'bus'}
             // data={{ name, busNumber, capacity }}
           />
         </div>
       </div>
     </div>
-  );
+  )
 }
